7091b0812f828d956c33b954c78941db3ed29582
technicca <cake3327@gmail.com>
2024-04-15 23:48:59 +0300

    postinstall scripts

---
+++ postinstall/riscv.sh
@@ -0,0 +1,8 @@
+yay -Sy
+yay -S riscv-gnu-toolchain-bin spike qemu-system-riscv64 --noconfirm 
+
+# might need to build manually:
+# yay -S riscv64-unknown-elf-gcc
+# https://github.com/riscv-software-src/riscv-pk
+# https://github.com/riscv-software-src/riscv-isa-sim
+# https://github.com/riscv-collab/riscv-gnu-toolchain
